9ddba4eeaaf3c8cb40d615229ff6eade32848ab1
supraja2030 <bsupraja7891@gmail.com>
2024-06-11 19:02:11 +0530

    second commit

---
+++ src/Common.java
@@ -0,0 +1,8 @@
+public class Common {
+    public static void main(String args[]){
+        Vehicle vehicle = new Vehicle();
+        Car car = new Car();
+        vehicle.engine();
+        car.engine();
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,5 @@
+public class Vehicle {
+    void engine(){
+        System.out.println(" Engine is common in any vehicle ");
+    }
+}
