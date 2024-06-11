fbd3a13031eb1f282f5c27b1d91e23eda68751bc
supraja2030 <bsupraja7891@gmail.com>
2024-06-11 17:31:57 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main( String args[]){
+        Human human1 = new Human("Rick", 65,67.2);
+        Human human2 = new Human("Max", 40,72.8);
+
+        human1.eat();
+        human2.drink();
+
+    }
+}
